8cfb0b866c4f6f0c9cea686fcba05ceb463cfcd7
Christian Meter <cmeter@googlemail.com>
2022-09-07 17:50:32 +0200

    Remove cuda support

--- detect.py
+++ detect.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import platform
 import shutil
+import sys
 import time
 from pathlib import Path
 
@@ -40,7 +41,7 @@ def detect(save_img=False):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = Darknet(cfg, imgsz).cuda()
+    model = Darknet(cfg, imgsz)
     model.load_state_dict(torch.load(weights[0], map_location=device)['model'])
     #model = attempt_load(weights, map_location=device)  # load FP32 model
     #imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
